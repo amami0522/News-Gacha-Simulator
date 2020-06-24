d27940132a298c1b8d384f74fe7a0b3e86dba716
amami0522 <31738@toyota.kosen-ac.jp>
2020-06-24 15:31:59 +0900

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+const port = 8000;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port)
+
+console.log("Listening on " + port);
---
+++ gacha.js
@@ -0,0 +1,4 @@
+'use strict'
+
+const NewsApi = require('newsapi');
+const newsapi = new NewsApi('71a614c0ed5046f4ad28a1a242543879');

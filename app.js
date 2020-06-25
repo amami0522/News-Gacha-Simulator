c96d598d7efdd7b57286b515737ab3072e63cc6b
amami0522 <31738@toyota.kosen-ac.jp>
2020-06-25 22:29:53 +0900

    入力した情報から動的にページを生成できるように

--- app.js
+++ app.js
@@ -2,13 +2,39 @@
 
 const http = require('http');
 const fs = require('fs');
-const port = 8000;
+const ejs = require('ejs');
+const qs = require('querystring');
+const server = http.createServer();
+var template = fs.readFileSync(__dirname + '/views/index.ejs', 'utf-8');
+const port = 8080;
 
-const server = http.createServer(function(req, res){
-    fs.readFile('index.html', 'utf-8', function(error, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
+var posts = [];
+
+function renderForm(posts, res){
+    var data = ejs.render(template, {
+        posts: posts
+    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+};
+
+server.on('request', function(req, res){
+    fs.readFile(__dirname + '/views/index.html', 'utf-8', function(err, data){
+        if(req.method === 'POST'){
+            req.data = "";
+            req.on('readable', function(){
+                req.data += req.read();
+            });
+            req.on('end', function() {
+                var query = qs.parse(req.data);
+                if(query.content !== "") posts.push(query.content);
+                renderForm(posts, res);
+            });
+        }
+        else{
+            renderForm(posts, res);
+        }
     });
 });
 
--- gacha.js
+++ gacha.js
@@ -1,15 +1,20 @@
 'use strict'
 
-const express = require('express');
-const app = express();
 const NewsApi = require('newsapi');
 const newsapi = new NewsApi('71a614c0ed5046f4ad28a1a242543879');
 
-app.get("/", (req, res) => {
-    newsapi.v2.topHeadlines({
-        country: 'jp',
-        pageSize: '100'
-    }).then(content => res.send(content));
+function takeGacha(){
+}
+
+var content = newsapi.v2.topHeadlines({
+    country: 'jp',
+    pageSize: '100',
+}).then(function(news){
+    news['articles'].forEach(function(item){
+        console.log(item.title);
+    });
 });
 
-const content = document.getElementById('content');
+module.exports = {
+    takeGacha
+};
